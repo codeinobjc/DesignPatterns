0ce15a3d17b22e063e6da404bb5e5a31be096bc0
Joyce Remilah <codeinobjc@gmail.com>
2018-09-20 15:32:33 -0700

    Initial Commit

---
+++ DesignPatterns/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DesignPatterns
+//
+//  Created by Keynes Paul on 9/20/18.
+//  Copyright © 2018 com.codeinobjc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DesignPatterns/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DesignPatterns
+//
+//  Created by Keynes Paul on 9/20/18.
+//  Copyright © 2018 com.codeinobjc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
